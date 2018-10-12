938f8eeb6a7b25fcef9cb57e555d4cfa3e6246e7
Adi563 <adimail@gmx.ch>
2018-10-12 22:40:57 +0200

    Loading of SRT implemented

--- Subtitles.Gui/Form1.Designer.cs
+++ Subtitles.Gui/Form1.Designer.cs
@@ -28,13 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialogSrt = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.listViewSrtItems = new System.Windows.Forms.ListView();
+            this.columnHeaderFrom = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderTo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderText = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // openFileDialogSrt
+            // 
+            this.openFileDialogSrt.FileName = "openFileDialog1";
+            this.openFileDialogSrt.Filter = "SRT|*.srt";
+            this.openFileDialogSrt.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialogSrt_FileOk);
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Location = new System.Drawing.Point(12, 14);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.Size = new System.Drawing.Size(435, 20);
+            this.textBoxFilePath.TabIndex = 0;
+            this.textBoxFilePath.TextChanged += new System.EventHandler(this.textBoxFilePath_TextChanged);
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(453, 12);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFile.TabIndex = 1;
+            this.buttonOpenFile.Text = "Open";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // listViewSrtItems
+            // 
+            this.listViewSrtItems.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewSrtItems.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderNumber,
+            this.columnHeaderFrom,
+            this.columnHeaderTo,
+            this.columnHeaderText});
+            this.listViewSrtItems.FullRowSelect = true;
+            this.listViewSrtItems.GridLines = true;
+            this.listViewSrtItems.Location = new System.Drawing.Point(12, 40);
+            this.listViewSrtItems.MultiSelect = false;
+            this.listViewSrtItems.Name = "listViewSrtItems";
+            this.listViewSrtItems.ShowGroups = false;
+            this.listViewSrtItems.Size = new System.Drawing.Size(776, 398);
+            this.listViewSrtItems.TabIndex = 2;
+            this.listViewSrtItems.UseCompatibleStateImageBehavior = false;
+            this.listViewSrtItems.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderFrom
+            // 
+            this.columnHeaderFrom.Text = "From";
+            // 
+            // columnHeaderTo
+            // 
+            this.columnHeaderTo.Text = "To";
+            // 
+            // columnHeaderText
+            // 
+            this.columnHeaderText.Text = "Text";
+            // 
+            // columnHeaderNumber
+            // 
+            this.columnHeaderNumber.Text = "#";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.listViewSrtItems);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Controls.Add(this.textBoxFilePath);
+            this.Name = "Form1";
+            this.Text = "SRT Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialogSrt;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.ListView listViewSrtItems;
+        private System.Windows.Forms.ColumnHeader columnHeaderNumber;
+        private System.Windows.Forms.ColumnHeader columnHeaderFrom;
+        private System.Windows.Forms.ColumnHeader columnHeaderTo;
+        private System.Windows.Forms.ColumnHeader columnHeaderText;
     }
 }
 
--- Subtitles.Gui/Form1.cs
+++ Subtitles.Gui/Form1.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace Subtitles.Gui
@@ -14,6 +9,32 @@ namespace Subtitles.Gui
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialogSrt.ShowDialog();
+        }
+
+        private void openFileDialogSrt_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            textBoxFilePath.Text = openFileDialogSrt.FileName;
+        }
+
+        private void textBoxFilePath_TextChanged(object sender, EventArgs e)
+        {
+            var fileExists = System.IO.File.Exists(textBoxFilePath.Text);
+
+            if (!fileExists) { return; }
+
+            var srtItems = SrtHandler.ReadSrt(System.IO.File.OpenRead(textBoxFilePath.Text));
+
+            listViewSrtItems.Items.Clear();
+            foreach (var srtItem in srtItems)
+            {
+                var listViewItem = new ListViewItem(new string[] { srtItem.Number.ToString(), srtItem.From.ToString(), srtItem.To.ToString(), srtItem.Text });
+                listViewSrtItems.Items.Add(listViewItem);
+            }
         }
     }
 }
